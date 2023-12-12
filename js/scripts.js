2db30a21f85de91db4d75f4e5a5aa77591df5fdd
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-12 23:03:41 +0700

    portofolio progres

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
-/*!
-* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+
 
 window.addEventListener('DOMContentLoaded', event => {
 
