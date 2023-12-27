83e134664c4bce37060ea2df2b0b6c217986e067
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-28 02:47:44 +0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,61 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
     var navbarShrink = function () {
+        var parallaxElements = document.querySelectorAll(".parallax-image");
+        var parallaxKiri = document.querySelector('.parallax-kiri');
+        var parallaxKanan = document.querySelector('.parallax-kanan');
+        parallaxElements.forEach(function (element) {
+          var distanceFromTop = element.getBoundingClientRect().top;
+          var screenHeight = window.innerHeight;
+
+            // Check if the parallax-kiri element exists
+  if (parallaxKiri) {
+    // Add 'show' class after a short delay (adjust the delay as needed)
+    setTimeout(function () {
+      parallaxKiri.classList.add('show');
+    }, 500); // 500 milliseconds delay
+  }
+  if (parallaxKanan) {
+    // Add 'show' class after a short delay (adjust the delay as needed)
+    setTimeout(function () {
+      parallaxKanan.classList.add('show');
+    }, 1000); // 500 milliseconds delay
+  }
+
+          if (distanceFromTop < screenHeight * 0.75) {
+            element.classList.add("parallax-show");
+          } else {
+            element.classList.remove("parallax-show");
+          }
+
+          if (distanceFromTop < screenHeight * 0.75) {
+            element.classList.add("parallax-show2");
+          } else {
+            element.classList.remove("parallax-show2");
+          }
+
+        });
+
+                var parallaxElements2 = document.querySelectorAll(".parallax-container");
+        parallaxElements2.forEach(function (element) {
+            var distanceFromTop = element.getBoundingClientRect().top;
+            var screenHeight = window.innerHeight;
+  
+            if (distanceFromTop < screenHeight * 0.75) {
+              element.classList.add("parallax-show");
+            } else {
+              element.classList.remove("parallax-show");
+            }
+  
+            if (distanceFromTop < screenHeight * 0.75) {
+              element.classList.add("parallax-show2");
+            } else {
+              element.classList.remove("parallax-show2");
+            }
+  
+          });
+
+
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
             return;
